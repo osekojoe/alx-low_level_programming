e3f90922be36816e8ce1c7b65f8e31c4799ffe63
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-10 08:17:41 -0800

    Printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing function
+ * Description: Function to print a string with 'print'
+ * Return: Returns zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\,");
+	return (0);
+}
