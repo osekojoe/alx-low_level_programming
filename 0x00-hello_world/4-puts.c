a8bd3b989953b5d459b8d0671bf141843403f292
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-10 08:04:09 -0800

    Printing with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A function that prints a string
+ * Description: A function that prints a string
+ * Return: Returns zero
+ */
+int main(void)
+{
+	fputs("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
