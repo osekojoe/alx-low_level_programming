c0ffbc998dda5f0b298482884cd9ee3c4bc758b1
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-11 13:10:07 -0800

    Evaluating the state of the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Assigns a random number to the variable n each iteration
+ * Description: Prints the last random number at the end of a string
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n % 10 > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5 \n", n, n % 10);
+	}
+	else if (n % 10 == 0)
+	{
+		printf("Last digit of %d is %d and is 0 \n", n, n % 10);
+	}
+	else if (n % 10 < 6 && n % 10 != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, n % 10);
+	}
+	return (0);
+}
