bf7515d407a9114768054cd361636ba8cea079a8
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-11 16:10:05 -0800

    Making 3 digit combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int num1, num2, num3;
 
-	for (num1 = 48; num1 <= 58; num1++)
+	for (num1 = 48; num1 < 58; num1++)
 	{
-		for (num2 = 49; num2 <= 58; num2++)
+		for (num2 = 49; num2 < 58; num2++)
 		{
-			for (num3 = 50; num3 <= 58; num3++)
+			for (num3 = 50; num3 < 58; num3++)
 			{
 				if (num3 > num2 && num2 > num1)
 				{
