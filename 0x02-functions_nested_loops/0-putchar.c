432750027f68597070e888d6e925103aeae507e1
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-15 07:28:49 -0700

    Printing a string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints a string
+ * Description: Prints a string followed by a new line
+ * Return: Always returns 0(sucess)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
