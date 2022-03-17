c31ce458263e29c3b96b6691bb7e6af035433ea1
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-17 12:06:40 -0700

    printing numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - integr
+ * Description: Prints an integer
+ * @n: integer
+ * Return: none
+ */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	} else
+	{
+		m = n;
+	}
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
+}
