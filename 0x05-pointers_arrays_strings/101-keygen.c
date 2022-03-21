e47a448af278a7a4cd1a0274389af1eb12a95f0e
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-21 15:40:04 -0700

    random password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pwd[100];
+	int i, j, k;
+
+	k = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		k += (pwd[i] + '0');
+		putchar(pwd[i] + '0');
+		if ((2772 - k) - '0' < 78)
+		{
+			j = 2772 - k - '0';
+			k += j;
+			putchar(j + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
