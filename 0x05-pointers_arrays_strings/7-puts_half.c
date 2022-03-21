2d1b15a40c82b2f9684b55fc7ee9c8409899887d
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-21 13:41:36 -0700

    printing half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 			_putchar(str[i]);
-	} else if (len % 2 == 0)
+	} else if (len % 2 != 0)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 			_putchar(str[n + 1]);
