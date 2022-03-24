7c777c5acbfc2d7b64ab363a33c732c98cf74f59
jdev <20902932+osekojoe@users.noreply.github.com>
2022-03-24 07:47:20 -0700

    printing an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer.
+* @n: integer
+* Return: integer
+*/
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n >= 0)
+	{
+		m = n;
+	}
+	else
+	{
+		m = -n;
+		_putchar('-');
+	}
+
+	if (m / 10)
+		print_number(m / 10);
+	
+	_putchar((m % 10) + '0');
+}
