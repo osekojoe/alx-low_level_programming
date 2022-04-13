d7dbd7ea3dbf538717d9c7c02f2bd47241745517
jdev <20902932+osekojoe@users.noreply.github.com>
2022-04-13 12:18:00 -0700

    performing simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/**
+* get_op_func - returns pointer to right op func
+* @s: string operator
+*
+* Return: the right function pointer
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		if (s != NULL && s[0] == ops[i].op[0] && !s[1])
+			return (ops[i].f);
+	}
+	return (NULL);
+}
