6a216ad67b3bc939a9459a2f7783a93ac291b208
jdev <20902932+osekojoe@users.noreply.github.com>
2022-04-13 11:55:48 -0700

    betty fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,7 +16,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && size != 0 && cmp != NULL)
 		while (i < size)
 		{
-			if(cmp(array[i]))
+			if (cmp(array[i]))
 				return (i);
 			i++;
 		}
