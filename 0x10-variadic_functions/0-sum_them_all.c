1b681c614bcb7510f21f4721aada7673c7091284
jdev <20902932+osekojoe@users.noreply.github.com>
2022-04-15 14:20:25 -0700

    removing standard lib

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,22 +1,22 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all -  sums all paramaters
- * @n: int, number of undefined arguments
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
  *
- * Return: sum of args, If n == 0, return 0
+ * Return: the resulting sum, If n == 0, return 0
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i;
+	int sum = 0;
 	va_list list;
-	unsigned int i, sum = 0;
 
 	va_start(list, n);
-	if (n != 0)
-		for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
-		;
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
 	va_end(list);
 
 	return (sum);
