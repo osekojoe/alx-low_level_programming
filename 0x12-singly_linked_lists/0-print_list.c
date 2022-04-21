ea78be9d98f65ae4279c7b458db85c4a1d51b843
jdev <20902932+osekojoe@users.noreply.github.com>
2022-04-21 10:47:04 -0700

    condition correction

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
 * print_list - prints all the elements of a list_t list.
@@ -12,7 +14,10 @@ size_t print_list(const list_t *h)
 
 	for (i = 0; h; i++)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (i);
