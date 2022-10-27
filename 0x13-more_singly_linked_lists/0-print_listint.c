a9e5542de0813a63367e4e20cc867468292467e8
joe <20902932+osekojoe@users.noreply.github.com>
2022-10-27 09:34:31 -0700

    function edit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,14 +1,15 @@
 #include "lists.h"
 
 /**
-* print_listint - prints all the elements of a list.
-* @h: list head.
-* Return: the number of nodes.
-*/
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t counter = 0; /* initialize elem counter */
-	const listint_t *current = h; /* position indicator */
+	size_t counter = 0;
+	const listint_t *current = h;
 
 	while (current != NULL)
 	{
