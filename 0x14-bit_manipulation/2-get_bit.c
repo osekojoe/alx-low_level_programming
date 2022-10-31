2d8502522a27c5dad63ad8f72e1a5edcf9dabc81
joe <20902932+osekojoe@users.noreply.github.com>
2022-10-31 12:14:24 -0700

    bug fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,14 +3,14 @@
 /**
 * get_bit - prints the binary representation of a number
 * @n: number
-* @index: bit index
+* @index: bit index starting from 0
 * Return: binary value
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
-	if (n == o && index < 64)
+	if (n == 0 && index < 64)
 		return (0);
 
 	for (i = 0; i <= 63; n >>= 1, i++)
