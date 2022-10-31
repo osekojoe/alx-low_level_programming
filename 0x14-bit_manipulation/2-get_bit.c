2e8c3daa18858af2748a214c01587a31c840d301
joe <20902932+osekojoe@users.noreply.github.com>
2022-10-31 11:31:59 -0700

    int to binary

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* get_bit - prints the binary representation of a number
+* @n: number
+* @index: bit index
+* Return: None
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == o && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+	return (-1);
+}
